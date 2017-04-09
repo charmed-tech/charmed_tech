15688d4f4103e98659597537ae7a94e600d34bca
CharmedSatyr <email@charmedsatyr.com>
2017-04-10 07:31:38 +0900

    +Additional PHP-side contact form validation

--- contact.php
+++ contact.php
@@ -7,21 +7,31 @@ $nameEntry = $subjectEntry = $emailEntry = $websiteEntry = $messageEntry = '';
 $myEmail  = "email@charmedsatyr.com";
 
 /* PHP cleaning functions */
-function check_input($data)
+function check_input($data, $problem='')
 {
     $data = trim($data); //Remove extra spaces
     $data = stripslashes($data); //Remove escape slashes
     $data = htmlspecialchars($data); //Remove special characters like brackets
+    if ($problem && strlen($data) == 0)
+    {
+      show_error($problem);
+    }
     return $data;
 }
 
 /* Check all form inputs using check_input function */
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-$nameEntry = check_input($_POST['nameEntry']);
-$subjectEntry  = check_input($_POST['subjectEntry']);
+$nameEntry = check_input($_POST['nameEntry'], "Enter your name."); //If name doesn't pass, return the show_error function saying "Enter your name"
+$subjectEntry  = check_input($_POST['subjectEntry'], "Write a subject.");
 $emailEntry    = check_input($_POST['emailEntry']);
 $websiteEntry  = check_input($_POST['websiteEntry']);
-$messageEntry = check_input($_POST['messageEntry']);
+$messageEntry = check_input($_POST['messageEntry'], "Write your comments.");
+}
+
+/* If e-mail is not valid show error message */
+if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-].+)/", $emailEntry))
+{
+    show_error("E-mail address not valid");
 }
 
 /* Let's prepare the message for the e-mail */
@@ -44,4 +54,18 @@ mail($myEmail, $subjectEntry, $messageSent);
 /* Redirect visitor to the thank you page */
 header('Location: thanks.html');
 
+function show_error($myError)
+{
+?>
+    <html>
+    <body>
+
+    <b>Please correct the following error:</b><br />
+    <?php echo $myError; ?>
+
+    </body>
+    </html>
+<?php
+exit();
+}
 ?>
